98343b81ccfcdac1eb26083d9434c392a3c802d5
Jclian <33828982+percent4@users.noreply.github.com>
2019-03-24 15:31:41 +0800

    Delete get_image.py

--- captcha_tagging/get_image.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-
-for i in range(1, 1000):
-    print(i)
-    url = 'http://www.yb.cc/code?t=%s' % str(1552640517290+i)
-    urllib.request.urlretrieve(url, '/Users/lianmingjie/Pictures/code%d.png' % i)
